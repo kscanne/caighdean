e9fbabaf6b6cedbe2ca841309a6c4dd722ff28c3
Kevin Scannell <kscanne@gmail.com>
2015-08-28 21:41:36 -0500

    check for trailing space in multi-*

--- maint/qa.sh
+++ maint/qa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-egrep -v '^[^ _]+(_[^ _]+)+ ([^_]+)$' ../multi*.txt
+egrep -v '^[^ _]+(_[^ _]+)+ ([^_]*[^_ ])$' ../multi*.txt
 egrep -v '^[^ _]+ ([^ _]+)( [^ _]+)*$' ../pairs*.txt ../spurious*.txt
