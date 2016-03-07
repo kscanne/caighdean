d89004ee89e9dfdcbb76c86082ab6c351287a98d
Kevin Scannell <kscanne@gmail.com>
2016-03-06 18:15:12 -0600

    test for bilingual detokenizer (EID style)

--- test/detokentest.sh
+++ test/detokentest.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 FREAMH=${HOME}/seal/caighdean
 TMPFILE=`mktemp`
+TMPFILE2=`mktemp`
 cd ${FREAMH}
+# monolingual detokenization, roundtrip
 cat test/detokentest-in.txt | bash alltokens.sh | perl detokenize.pl > $TMPFILE
-diff -u test/detokentest-in.txt $TMPFILE
-eval "rm -f $TMPFILE; exit $?"
+# bilingual detokenization
+cat test/testin.txt | bash tiomanai.sh | perl detokenize.pl > $TMPFILE2
+diff -u test/detokentest-in.txt $TMPFILE && diff -u test/detokentest-eid.txt $TMPFILE2
+eval "rm -f $TMPFILE $TMPFILE2; exit $?"
