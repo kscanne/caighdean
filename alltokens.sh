172049337aa7b61e63ffac35349569b3eabca28a
Kevin Scannell <kscanne@gmail.com>
2016-03-13 17:12:20 -0500

    more comment

--- alltokens.sh
+++ alltokens.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# This script is used throughout this repo for tokenization
+# and also in the seal/idirlamha/xx/freq directories...
+# If we do change the characters below, should also keep
+# the stuff in gaeilge/ngram in sync!
 # don't use ${HOME} since web service uses this
 FREAMH=/home/kps/seal/caighdean
 perl ${FREAMH}/alltokens.pl "-‑‐" "0-9ʼ’'#_@"
