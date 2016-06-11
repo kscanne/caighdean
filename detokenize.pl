8cdaa5ab311f27066abb7e943dbae198ede80cd3
Kevin Scannell <kscanne@gmail.com>
2016-06-11 13:22:25 -0500

    detokenizer for parallel HTML output with mouseover

--- detokenize.pl
+++ detokenize.pl
@@ -12,16 +12,26 @@ if ($#ARGV != 0 or $ARGV[0] !~ m/^-[stfap]$/) {
     die "Usage: $0 [-s|-t|-f|-a|-p]\n  -s: source language only\n  -t: target language only\n  -f: focloir.ie NEID output\n  -a: mouseover annotations in HTML\n  -p: parallel text in HTML";
 }
 
+# stuff for -p
+my $opener='<td width="50%" valign="top"><div class="textcol">';
+my $closer='</div></td>';
+my $rhs=$opener;
+my $counter=0;
+
 my $dispatch = {
 	'-s' => sub { (my $s, my $t) = @_; return $s; },
 	'-t' => sub { (my $s, my $t) = @_; return $t; },
 	'-f' => sub { (my $s, my $t) = @_; $t =~ s/ /_/g; return "^$s^ $t"; },
 	'-a' => sub { (my $s, my $t) = @_; return "<span class=\"tooltip\">$s<span>$t</span></span>"; },
+	'-p' => sub { (my $s, my $t) = @_; return "<label class=\"w$counter\">$s</label>"; },
+	'-p2' => sub { (my $s, my $t) = @_; return "<label class=\"w$counter\">$t</label>"; },
 };
 # set this flag true if no need for space before next token
 # For example, start of doc, left parens, brackets
 my $suppress = 1;
 my $ascii_double_quote_parity = 0;
+
+print "$opener\n" if ($ARGV[0] eq '-p');
 
 # always assume format of input is the same as output of tiomanai.sh,
 # i.e. a pair of source/target tokens separated by '=>' on each line
@@ -31,22 +41,33 @@ while (<STDIN>) {
 	if ($s eq '\n') {
 		$suppress = 1;
 		print "\n";
+		$rhs .= "\n";
 	}
 	else {
 		if ($s eq '"') {
 			$suppress = 1 if ($ascii_double_quote_parity==1);
 			$ascii_double_quote_parity = 1 - $ascii_double_quote_parity;
 		}
-		print " " unless ($suppress == 1 or $s =~ /^([.,\/;”:!?%})]|<\/[^>]*>)$/);
+		unless ($suppress == 1 or $s =~ /^([.,\/;”:!?%})]|<\/[^>]*>)$/) {
+			print " ";
+			$rhs .= " ";
+		}
 		$suppress = (($s =~ /^([“\/\$(\[#{]|<[^\/>][^>]*>)$/) or ($s eq '"' and $ascii_double_quote_parity==1));
 		if ($s eq $t) {
 			print $s;
+			$rhs .= $s;
 		}
 		else {
 			print $dispatch->{$ARGV[0]}->($s,$t);
+			$rhs .= $dispatch->{'-p2'}->($s,$t);
 			$suppress = 0;
+			$counter++;
 		}
 	}
+}
+
+if ($ARGV[0] eq '-p') {
+	print "$closer\n$rhs\n$closer\n";
 }
 
 exit 0;
