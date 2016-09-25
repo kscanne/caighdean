e30f0c448c4f72b52a301105ffd9da97c267d872
Kevin Scannell <kscanne@gmail.com>
2016-09-25 07:49:21 -0500

    make tiomanai.sh runnable from anywhere

--- tiomanai.sh
+++ tiomanai.sh
@@ -2,4 +2,6 @@
 # naively tokenize ASCII and unicode apostrophes as boundary chars;
 # normalize to ASCII in caighdean.pl
 # and keep as boundary chars only for words appearing in lexicon
+FREAMH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${FREAMH}
 perl preproc.pl | bash alltokens.sh | perl nasc.pl $@ | perl caighdean.pl $@
