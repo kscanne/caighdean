f72a72f75dc26e7b36ea4ec56c164633ffb59988
Kevin Scannell <kscanne@gmail.com>
2017-01-28 10:08:18 -0600

    add java client to test suite

--- test/clienttest.sh
+++ test/clienttest.sh
@@ -6,10 +6,15 @@ FREAMH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 # third arg is name of language interpreter (bash, python, perl, ...)
 # fourth arg is source language code (ga, gd, or gv)
 testoneclient() {
-	EXTENSION=`echo "${3}" | sed 's/^bash$/.sh/; s/^perl$/.pl/; s/^python$/.py/; s/^ruby$/.rb/'`
+	EXTENSION=`echo "${3}" | sed 's/^bash$/.sh/; s/^perl$/.pl/; s/^python$/.py/; s/^ruby$/.rb/; s/^java$//'`
+	PROGRAM='client'
+	if [ "${3}" = "" ]
+	then
+		PROGRAM='./client'
+	fi
 	TMPFILE2=`mktemp`
 	cd ${FREAMH}/clients
-	cat "../test/testin${1}.txt" | ${3} "./client${EXTENSION}" ${4} > $TMPFILE2
+	cat "../test/testin${1}.txt" | ${3} "${PROGRAM}${EXTENSION}" ${4} > $TMPFILE2
 	diff -u "${2}" "${TMPFILE2}"
 	# eval ensures $? will contain exit val of the diff
 	eval "rm -f $TMPFILE2; return $?"
@@ -22,7 +27,7 @@ testonelang() {
 	TMPFILE=`mktemp`
 	cd ${FREAMH}
 	cat "test/testin${1}.txt" | bash tiomanai.sh ${2} > $TMPFILE
-	testoneclient "${1}" "$TMPFILE" 'bash' "${3}" && testoneclient "${1}" "$TMPFILE" 'perl' "${3}" && testoneclient "${1}" "$TMPFILE" 'python' "${3}" && testoneclient "${1}" "$TMPFILE" 'ruby' "${3}" && testoneclient "${1}" "$TMPFILE" '' "${3}"
+	testoneclient "${1}" "$TMPFILE" 'bash' "${3}" && testoneclient "${1}" "$TMPFILE" 'perl' "${3}" && testoneclient "${1}" "$TMPFILE" 'python' "${3}" && testoneclient "${1}" "$TMPFILE" 'ruby' "${3}" && testoneclient "${1}" "$TMPFILE" '' "${3}" && testoneclient "${1}" "$TMPFILE" 'java' "${3}"
 	eval "rm -f $TMPFILE; return $?"
 }
 
