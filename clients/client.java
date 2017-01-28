09a1e173a979bffe10fc6c40bd24d8d564b10d0a
Kevin Scannell <kscanne@gmail.com>
2017-01-28 10:04:11 -0600

    finish java client

--- clients/client.java
+++ clients/client.java
@@ -7,26 +7,80 @@ import java.net.URLEncoder;
 
 public class client {
 
+  public static void fatalError(String msg) {
+      System.err.println(msg);
+      System.exit(1);
+  }
+
+  // when called, index i points to first char *after* opening double quote
+  // return the index of the first char *after* closing double quote
+  public static int substringOutput(String json, int i) {
+    StringBuilder sb = new StringBuilder();
+    int len = json.length();
+    int j = i;
+    while (j < len && json.charAt(j) != '"') {
+      if (json.charAt(j)=='\\') j++;
+      sb.append(json.charAt(j));
+      j++;
+    }
+    if (j >= len) fatalError("Malformed JSON.");
+    System.out.print(sb.toString());
+    return j+1;
+  }
+
+  // a real client should use an actual JSON parser;
+  // doing it this way for the sake of a dependency-free client
+  public static void jsonToOutput(String json) {
+    int len = json.length();
+    int i=0;
+    while (i < len && Character.isWhitespace(json.charAt(i))) i++;
+    if (i==len || json.charAt(i)!='[') fatalError("Malformed JSON.");
+    i++;
+    while (i < len && Character.isWhitespace(json.charAt(i))) i++;
+    while (i < len && json.charAt(i)=='[') {
+      i++;
+      while (i < len && Character.isWhitespace(json.charAt(i))) i++;
+      if (i==len || json.charAt(i)!='"') fatalError("Malformed JSON.");
+      i++;
+      i = substringOutput(json,i);
+      System.out.print(" => ");
+      while (i < len && Character.isWhitespace(json.charAt(i))) i++;
+      if (i==len || json.charAt(i)!=',') fatalError("Malformed JSON.");
+      i++;
+      while (i < len && Character.isWhitespace(json.charAt(i))) i++;
+      if (i==len || json.charAt(i)!='"') fatalError("Malformed JSON.");
+      i++;
+      i = substringOutput(json,i);
+      System.out.print("\n");
+      while (i < len && Character.isWhitespace(json.charAt(i))) i++;
+      if (i==len || json.charAt(i)!=']') fatalError("Malformed JSON.");
+      i++;
+      while (i < len && Character.isWhitespace(json.charAt(i))) i++;
+      if (i < len && json.charAt(i)==']') break;
+      if (i==len || json.charAt(i)!=',') fatalError("Malformed JSON.");
+      i++; 
+      while (i < len && Character.isWhitespace(json.charAt(i))) i++;
+    }
+  }
+
   public static void main(String[] args) throws Exception {
 
-    if (args.length != 1) {
-      System.err.println("Usage:");
-      System.exit(1);
-    }
+    if (args.length != 1)
+      fatalError("Usage: java client [ga|gd|gv]");
 
-    if (!args[0].equals("ga") && !args[0].equals("gd") && !args[0].equals("gv")) {
-      System.err.println("Usage:");
-      System.exit(1);
-    }
+    if (!args[0].equals("ga") && !args[0].equals("gd") && !args[0].equals("gv"))
+      fatalError("Usage: java client [ga|gd|gv]");
 
     String stdInLine;
-    String slurp = "";
+    StringBuffer slurp = new StringBuffer();
     BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
     while ((stdInLine = stdIn.readLine()) != null) {
-      slurp += stdInLine;
+      slurp.append(stdInLine);
+      // maybe a pain if stdin doesn't end in a newline
+      slurp.append("\n");
     }
 
-    String urlParameters = "foinse=" + URLEncoder.encode(args[0],"UTF-8") + "&teacs=" + URLEncoder.encode(slurp, "UTF-8");
+    String urlParameters = "foinse=" + URLEncoder.encode(args[0],"UTF-8") + "&teacs=" + URLEncoder.encode(slurp.toString(), "UTF-8");
     byte[] postData = urlParameters.getBytes("UTF-8");
 
     String url = "http://borel.slu.edu/cgi-bin/seirbhis3.cgi";
@@ -44,29 +98,18 @@ public class client {
     wr.close();
 
     int responseCode = conn.getResponseCode();
-    if (responseCode != 200) {
-      System.err.println("HTTP Error code " + Integer.toString(responseCode));
-      System.exit(1);
-    }
+    if (responseCode != 200)
+      fatalError("HTTP Error code " + Integer.toString(responseCode));
 
     BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
     String inputLine;
     StringBuffer response = new StringBuffer();
-
     while ((inputLine = in.readLine()) != null) {
       response.append(inputLine);
     }
     in.close();
 
-    String resp = response.toString();
-// parse resp as JSON
-
-// verify it parses correctly
-// varify it gives an array!
-
-// loop over the array, pick out source/target from each and print em
-
-    System.out.println(resp);
+    jsonToOutput(response.toString());
   }
 
 }
