615c68ee332a124715a829a69d3cada1e0366a3d
Kevin Scannell <kscanne@gmail.com>
2023-07-23 22:25:24 +0000

    Python2 -> Python3, is mithid

--- clients/client.py
+++ clients/client.py
@@ -1,44 +1,36 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# json is part of Python 2.6, not 2.5.  
-import urllib
-import urllib2
+import urllib.parse
+import urllib.error
+import urllib.request
 import json
 import sys
-import socket
-
-# hack to allow piping of output w/o errors
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 def kprint(s):
-	print s.encode('utf-8')
+	print(s)
 
 def make_request(param_dict):
-	params = urllib.urlencode(param_dict)
-	headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/json'}
+	params = urllib.parse.urlencode(param_dict)
+	paramsdata = params.encode("ascii")
+	headerdict = {'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/json'}
+	url = 'https://cadhan.com/api/intergaelic/3.0'
+	req = urllib.request.Request(url, paramsdata, headers=headerdict)
 	try:
-		req = urllib2.Request('https://cadhan.com/api/intergaelic/3.0', params, headers)
-	except URLError as e:
+		with urllib.request.urlopen(req) as response:
+			try:
+				array_of_pairs = json.loads(response.read())
+			except ValueError:
+				kprint('Malformed JSON returned from the server')
+				return []
+			return array_of_pairs
+	except urllib.error.URLError as e:
 		kprint('Unable to connect with the server: ' + e.reason)
 		return []
-	except urllib2.HTTPError as e:
+	except urllib.error.HTTPError as e:
 		kprint('HTTP error from server: ' + e.code)
 		return []
-	else: # we're good
-		r1 = urllib2.urlopen(req)
-		try:
-			array_of_pairs = json.loads(r1.read())
-		except ValueError:
-			kprint('Malformed JSON returned from the server')
-			return []
-		return array_of_pairs
 	
 def usage():
 	kprint('Usage: python client.py [ga|gd|gv]')
 
-# ok to just read in as byte streams for now; if we eventually want UTF-8,
-# can do sys.stdin = codecs.getreader('utf-8')(sys.stdin) 
 def main():
 	if len(sys.argv) != 2:
 		usage()
